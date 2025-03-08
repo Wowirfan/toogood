621dc6eab898b71c6f6a95372dfd7cb92d251a86
Mohd Irfan <muhammadirfan467248@gmail.com>
2025-03-08 19:03:58 +0530

    Auto update

--- script.js
+++ script.js
@@ -48,4 +48,18 @@ function loadingvideocon(){
         
     })
 }
-loadingvideocon()
+
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+    
+    })
+    
+})
+document.querySelector("#child1").addEventListener("mouseenter",function(){
+    gsap.to("#cursor",{
+        transform: 'translate(-50%,-50%) scale(1)'
+    })
+})
