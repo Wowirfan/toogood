f1ef020d3a7120bf40fb514d4a3b63c2d4b0e3dc
Mohd Irfan <muhammadirfan467248@gmail.com>
2025-03-09 13:56:11 +0530

    Auto update

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function locomotiveAnimation(){
 // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
 const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
+  el: document.querySelector("#"),
   smooth: true
 });
 // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+// tell ScrollTrigger to use these proxy methods for the "#" element since Locomotive Scroll is hijacking things
 ScrollTrigger.scrollerProxy(".smooth-scroll", {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
