ac686073bbb48d58b8fda196d6be62fc7d97c795
Mohd Irfan <muhammadirfan467248@gmail.com>
2025-03-09 14:12:21 +0530

    Auto update

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ ScrollTrigger.refresh();
 }
 locomotiveAnimation()
 gsap.to("#nav-part1 svg",{
-    transform:"translateY"
+    transform:"translateY(-100)"
 })
 function videoconAnimation(){
     var videocon = document.querySelector("#video-container")
